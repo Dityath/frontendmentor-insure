eecc040d2c68fa45ddf9062d43f726a4d40424ea
dityath <dityaathallah107@gmail.com>
2021-10-17 01:17:49 +0700

    navbar responsiveness finish

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,11 +1,32 @@
 <template>
-  <nav class="fixed px-28 py-5 flex justify-between items-center bg-white w-full z-50">
+  <nav class="
+    flex justify-between px-4 py-5 items-center fixed w-full bg-white z-50
+    sm:px-14
+    lg:px-28
+  ">
     <img class="" src="../assets/images/logo.svg" alt="logo">
-    <div class="font-karla flex justify-center gap-8 items-center text-primary-grayBlue">
-      <a href="" class="transition duration-200 hover:text-primary-darkViolet">HOW WE WORK</a>
-      <a href="" class="transition duration-200 hover:text-primary-darkViolet">BLOG</a>
-      <a href="" class="transition duration-200 hover:text-primary-darkViolet">ACCOUNT</a>
-      <button class="text-primary-darkViolet p-2 px-6 border-solid border-2 border-primary-darkViolet transition duration-200 hover:bg-primary-darkViolet hover:text-neutral-veryLightgray">VIEW PLANS</button>
+
+    <img class="cursor-pointer lg:hidden" src="../assets/images/icon-hamburger.svg">
+
+    <!-- large -->
+    <div class="hidden
+      font-karla lg:flex justify-center gap-8 items-center text-primary-grayBlue
+    ">
+      <a href="" class="transition duration-200 hover:text-primary-darkViolet">
+        HOW WE WORK
+      </a>
+      <a href="" class="transition duration-200 hover:text-primary-darkViolet">
+        BLOG
+      </a>
+      <a href="" class="transition duration-200 hover:text-primary-darkViolet">
+        ACCOUNT
+      </a>
+      <button class="
+        text-primary-darkViolet p-2 px-6 border-solid border-2 border-primary-darkViolet transition duration-200 
+        hover:bg-primary-darkViolet hover:text-neutral-veryLightgray
+      ">
+        VIEW PLANS
+      </button>
     </div>
   </nav>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,7 @@
 <template>
   <div>
     <Navbar />
-    <section class="pt-52 px-28 pb-24 bg-primary-darkViolet text-neutral-veryLightgray flex justify-between">
-      <div class="w-1/2 pr-5">
-        <hr class="w-44">
-        <h1 class="font-dm-serif text-6xl mt-10">Humanizing your insurance.</h1>
-        <p class="font-karla mt-5">Get your life insurance coverage easier and faster. We blend our expertise 
-  and technology to help you find the plan that’s right for you. Ensure you 
-  and your loved ones are protected.</p>
-        <button class="font-karla text-neutral-veryLightgray p-2 px-6 mt-8 border-solid border-2 border-neutral-veryLightgray transition duration-200 hover:bg-neutral-veryLightgray hover:text-primary-darkViolet">VIEW PLANS</button>
-      </div>
-      <img class="absolute right-28" src="../assets/images/image-intro-desktop.jpg" alt="First image">
-
-      <img class="absolute right-0 top-0" src="../assets/images/bg-pattern-intro-right-desktop.svg">
-
-    </section>
+    <LargeFirst />
     <section class=" mt-96 mb-20 px-28 text-neutral-veryDarkViolet">
 
       <img class="absolute left-0 top-96 mt-32 w-48" src="../assets/images/bg-pattern-intro-left-desktop.svg">
