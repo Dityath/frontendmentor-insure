717ca6f59e2c2fd0fb35264a43f4f0be3e2eac9b
dityath <dityaathallah107@gmail.com>
2021-10-17 13:21:53 +0700

    website finished

--- components/LargeFirst.vue
+++ components/LargeFirst.vue
@@ -6,10 +6,10 @@
     <div class="w-1/2 pr-5">
       <hr class="w-44">
       <h1 class="font-dm-serif text-6xl 2xl:text-8xl mt-10">Humanizing your insurance.</h1>
-      <p class="font-karla mt-5 2xl:text-xl">Get your life insurance coverage easier and faster. We blend our expertise 
+      <p class="font-karla mt-5 2xl:mt-10 2xl:text-xl">Get your life insurance coverage easier and faster. We blend our expertise 
 and technology to help you find the plan that’s right for you. Ensure you 
 and your loved ones are protected.</p>
-      <button class="font-karla 2xl:text-xl text-neutral-veryLightgray p-2 px-6 mt-8 border-solid border-2 border-neutral-veryLightgray transition duration-200 hover:bg-neutral-veryLightgray hover:text-primary-darkViolet">VIEW PLANS</button>
+      <button class="font-karla 2xl:text-xl text-neutral-veryLightgray p-2 px-6 mt-8 2xl:mt-12 border-solid border-2 border-neutral-veryLightgray transition duration-200 hover:bg-neutral-veryLightgray hover:text-primary-darkViolet">VIEW PLANS</button>
     </div>
     <img class="absolute right-28 2xl:right-60 w-96 xl:w-auto" src="../assets/images/image-intro-desktop.jpg" alt="First image">
 
--- components/LargeSecond.vue
+++ components/LargeSecond.vue
@@ -1,11 +1,11 @@
 <template>
-  <section class="hidden lg:block mt-96 mb-20 px-28 2xl:px-60 text-neutral-veryDarkViolet">
+  <section class="hidden lg:block mt-96 2xl:mt-64 mb-20 px-28 2xl:px-60 text-neutral-veryDarkViolet">
 
     <img class="absolute left-0 top-96 mt-32 w-48" src="../assets/images/bg-pattern-intro-left-desktop.svg">
 
     <hr class="w-44">
-    <h1 class="font-dm-serif text-6xl 2xl:text-8xl mt-10">We’re different</h1>
-    <div class="flex gap-5 2xl:gap-20 justify-between mt-20 font-karla text-neutral-darkGrayBlue">
+    <h1 class="font-dm-serif text-6xl 2xl:text-8xl mt-10 2xl:mt-20">We’re different</h1>
+    <div class="flex gap-5 2xl:gap-20 justify-between mt-20 2xl:mt-32 font-karla text-neutral-darkGrayBlue">
       <div class="">
         <img src="../assets/images/icon-snappy-process.svg" alt="Flash Icon">
         <h5 class="text-neutral-veryDarkViolet font-dm-serif text-3xl 2xl:text-5xl mt-7">Snappy Process</h5>
